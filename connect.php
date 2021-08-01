6954da3a8e7a051757102b7089c604ec14e99cce
Okogikam <okogikam@gmail.com>
2021-08-01 11:22:52 +0800

    delete yang tidk perlu

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hn = 'localhost';
-    $un = 'habb3595_okogikam';
-    $pw = 'jkluio789';
-    $db = 'habb3595_anime';
-    $conn = new mysqli($hn, $un, $pw, $db);
-    if($conn->connect_error)die("Akses Gagal : ". $conn->connect_error);
-?>
